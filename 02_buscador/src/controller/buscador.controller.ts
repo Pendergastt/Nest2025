55a63c8e0edf516a6942bc6d60a0c8c48966a847
pendergastt <penddergas@gmail.com>
2025-05-20 14:12:54 +0200

    02 subido

---
+++ 02_buscador/src/controller/buscador.controller.ts
@@ -0,0 +1,29 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  
+} from '@nestjs/common';
+
+import { BuscadorService } from 'src/service/buscador.service';
+import { Item } from 'src/model/item';
+
+@Controller('buscador')
+export class BuscadorController {
+  constructor(private readonly buscadorService: BuscadorService) {}
+
+  @Get("buscar/:tematica")
+  buscar(@Param("tematica")tematica:string):Item[]{
+    return this.buscadorService.buscar(tematica);
+  }
+
+  @Post("alta")
+  altaItem(@Body()item:Item):void{
+
+    return this.buscadorService.alta(item);
+
+  }
+
+}
--- 02_buscador/src/service/buscador.service.ts
+++ 02_buscador/src/service/buscador.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { Item } from 'src/model/item';
 
 @Injectable()
 export class BuscadorService {
 
 // Esta es lo que llamamos la capa de negocio
+
+repositorio:Item[]=[
+    new Item("http://www.fnac.com","libros","libros de todo tipo"),
+    new Item("http://www.game.es","juegos","juegos varios"),
+    new Item("http://www.retro.com","juegos","juegos retro"),
+    new Item("http://www.casadellibro.es","libros","libros en cualquier idioma"),
+    new Item("http://www.mytravel","viajes","viajes por todo el mundo")
+  ];
+
+
+  buscar(tematica:string):Item[]{
+    return this.repositorio.filter(it=>it.tematica==tematica); //objetos "it" cuya it.temática sea igual que la tematica que le pedimos
+  }
+
+  alta(item:Item):void{
+    this.repositorio.push(item);
+  }
+
+
 
 }
