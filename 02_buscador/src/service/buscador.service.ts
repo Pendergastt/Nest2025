9dd1aba6def800d6a1348a6084104fd69175c21b
pendergastt <penddergas@gmail.com>
2025-05-20 13:41:52 +0200

    creado service

--- 02_buscador/src/service/buscador.service.ts
+++ 02_buscador/src/service/buscador.service.ts
@@ -1,26 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { CreateBuscadorDto } from './dto/create-buscador.dto';
-import { UpdateBuscadorDto } from './dto/update-buscador.dto';
 
 @Injectable()
 export class BuscadorService {
-  create(createBuscadorDto: CreateBuscadorDto) {
-    return 'This action adds a new buscador';
-  }
 
-  findAll() {
-    return `This action returns all buscadors`;
-  }
+// Esta es lo que llamamos la capa de negocio
 
-  findOne(id: number) {
-    return `This action returns a #id buscador`;
-  }
-
-  update(id: number, updateBuscadorDto: UpdateBuscadorDto) {
-    return `This action updates a #id buscador`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #id buscador`;
-  }
 }
