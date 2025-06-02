710893197e00295807a70a1d883921536e967549
pendergastt <penddergas@gmail.com>
2025-06-02 14:14:41 +0200

    feat: add buscarAlumnos method to AlumnosController for retrieving students by course

--- 08_matriculacion_alumno/src/controller/alumnos.controller.ts
+++ 08_matriculacion_alumno/src/controller/alumnos.controller.ts
@@ -7,11 +7,21 @@ import {
   Patch,
   Post,
 } from '@nestjs/common';
+import { Alumno } from 'src/model/Alumno';
 import { AlumnosService } from 'src/service/alumnos.service';
 
 @Controller('alumnos')
 export class AlumnosController {
   constructor(private readonly alumnosService: AlumnosService) {}
+@Get("nomat/:curso")
+buscarAlumnos(@Param("curso") curso:number):Promise<Alumno[]>{
+  return this.alumnosService.buscarAlumno(curso)
+
+
+}
+
+agregarAlumnos(){}
+
 
 
 }
