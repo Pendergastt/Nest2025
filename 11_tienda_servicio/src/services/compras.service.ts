b034ca56ec9497f1e23d8e63df39c9d60b30f1e8
pendergastt <penddergas@gmail.com>
2025-06-16 12:27:30 +0200

    asf

--- 11_tienda_servicio/src/services/compras.service.ts
+++ 11_tienda_servicio/src/services/compras.service.ts
@@ -9,13 +9,22 @@ export class ComprasService {
     throw new Error('Method not implemented.');
   }
   constructor(
-  ){}
+  ){ this.autenticarUsuario()}
 
+
+  token:string;
   urlGlobal="http://localhost:3000"
+
+  username:string="usuario1";
+  password:string="usuario1";
+  urlRecursos:string="http://localhost:3000/tienda";
+  urlAutenticar:string="http://localhost:3000/autenticacion/login";
+
+  
 
 async mostrarPedido(precioMin:number,precioMax:number):Promise<ProductoDto[]>{
 
-  const response:any = await axios.get(`${this.urlGlobal}/tienda/productos`);
+  const response:any = await axios.get(`${this.urlGlobal}/tienda/productos`,{headers:{Authorization:`Bearer ${this.token}`}});
   const resultado = response.data.filter(p=>p.precioUnitario>=precioMin && p.precioUnitario<=precioMax);
   const producto:ProductoDto[] = resultado.map(
     p=>{
@@ -40,12 +49,20 @@ async mostrarPedido(precioMin:number,precioMax:number):Promise<ProductoDto[]>{
 
 async hacerPedido(pedido:PedidoDto):Promise<boolean>{
   try{
-  await axios.post(`${this.urlGlobal}/altaPedido`,pedido);
+  await axios.post(`${this.urlGlobal}/altaPedido`,pedido,{headers:{Authorization:`Bearer ${this.token}`}});
   return true;
   }catch(err){
     return false;
   }
   }
+
+async autenticarUsuario():Promise<any>{
+   const response = await axios.post(this.urlAutenticar,{username:this.username,password:this.password});
+    this.token = response.data.valorToken
+    console.log(this.token);
+
+}
+
 
 }
 
